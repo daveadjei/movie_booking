f27699e91116788e8795e88c1ce1d2cdc1e5c8bf
Fadz <fadzilah.whyte@aol.co.uk>
2018-02-16 10:27:59 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Movie_Booking</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+
+
+    <h1>Movie Booking for Geeks</h1>
+
+    <form method="POST" action="">
+    Movie <input type="text"><br>
+    Date <input type="date"><br>
+    Time <input type="time"><br>
+    Date of Birth <input type="date"><br>
+    Seat Number <input type="number"><br>
+    <hr>
+    <h1> Guest</h1> <br>
+    Name <input type="text"><br>
+    Email <input type="email"><br>
+    Mobile <input type="number"><br>
+    <hr>
+<button type="submit" value="submit">Show me the money!</button>
+
+
+    </form>
+</body>
+</html>
